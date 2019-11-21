0a53e2efafdb4d26f946476fe8f9a79d1d4bfa7b
lleonesouza <aliencode.host@gmail.com>
2019-11-21 12:04:22 -0300

    Updates

---
+++ precache-manifest.0499ddfc8b66e9eb60cfc007887f4e1f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b0efd1c6eb30f30717387305fe4d8f",
+    "url": "/cron/index.html"
+  },
+  {
+    "revision": "2e5cfddb7a54a0ef8350",
+    "url": "/cron/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f105fcce5aca850a4ed7",
+    "url": "/cron/static/js/2.b5b7facb.chunk.js"
+  },
+  {
+    "revision": "2e5cfddb7a54a0ef8350",
+    "url": "/cron/static/js/main.d5c400bc.chunk.js"
+  },
+  {
+    "revision": "5bec43aa90e8a942b450",
+    "url": "/cron/static/js/runtime-main.e57d8959.js"
+  },
+  {
+    "revision": "ad2a472e14a9eeee530f6be0bdefc885",
+    "url": "/cron/static/media/test.ad2a472e.png"
+  }
+]);
